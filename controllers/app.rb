f780d5a222298394cb579752b80ca88c4ad53db4
Guilherme Henrique <gjhenrique@gmail.com>
2016-03-29 22:52:08 -0200

    Layout cleaning and menu

--- controllers/app.rb
+++ controllers/app.rb
@@ -1,11 +1,11 @@
 get '/' do
-  university = University.random
-  meals_response university
+  @university = University.random
+  meals_response @university
 end
 
 get '/weekly/:university_name' do
-  university = University.where(name: params[:university_name]).first
-  meals_response university
+  @university = University.where(name: params[:university_name]).first
+  meals_response @university
 end
 
 def meals_response(university)
