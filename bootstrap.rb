a40c5d4dc74f9fe1cda4a789d5c39d4c78a03b46
Guilherme Henrique <gjhenrique@gmail.com>
2017-10-22 18:17:41 -0200

    Remove views from this project

--- bootstrap.rb
+++ bootstrap.rb
@@ -4,14 +4,7 @@
 require 'sinatra'
 require 'sinatra/respond_with'
 require 'sinatra/activerecord'
-require 'sinatra/asset_pipeline'
 require 'sinatra/i18n'
-require 'sinatra/capture'
-require 'sinatra/flash'
-
-require 'rails-assets-pure'
-require 'rails-assets-css-hamburgers'
-require 'font-awesome-sass'
 
 require 'dotenv'
 
@@ -19,7 +12,6 @@ Dir.glob('./models/*.rb') { |file| require file }
 Dir.glob('./jobs/*.rb') { |file| require file }
 Dir.glob('./controllers/*.rb') { |file| require file }
 
-require './views/view_helper.rb'
 require './parsers/parsers'
 
 require './app.rb'
--- controllers/helper.rb
+++
@@ -1,9 +0,0 @@
-helpers do
-  def group_meals_by_date(meals)
-    grouped_meals = meals.group_by(&:meal_date)
-
-    grouped_meals.each do |meal_date, list_meals|
-      list_meals.sort! { |meal| meal.period.index }
-    end
-  end
-end
