fa558cd60340ee1682cebe3998cd93c27b59d549
Guilherme Henrique <gjhenrique@gmail.com>
2017-10-22 17:59:55 -0200

    Remove some useless comments from deploy.rb

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -10,7 +10,6 @@ set :repo_url, 'https://github.com/gjhenrique/BandejaoServer.git'
 
 set :nginx_use_ssl, true if fetch(:stage) == :production
 
-# Default deploy_to directory is /var/www/my_app_name
 set :default_env, {
   'RACK_ENV' => 'production'
 }
@@ -18,26 +17,9 @@ set :default_env, {
 set :deploy_to, '/home/deploy/easy_meal_deploy'
 
 set :rails_env, :production
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'db-prod', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 set :puma_workers, 1
 after 'puma:check', 'puma:nginx_config'
 after 'deploy:migrate', 'deploy:seed'
