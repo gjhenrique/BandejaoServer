b69ccebe658a5c211b2c494f9e8f033beb8a6dde
Guilherme Henrique <gjhenrique@gmail.com>
2017-10-29 02:24:14 -0200

    Dont depend so much on sinatra

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
-configure do
-  set :root, File.dirname(__FILE__)
-  set :is_production, (ENV['RACK_ENV'] == 'production')
+module App
+  module_function
 
-  set :locales, Dir[File.join(settings.root, 'config', 'locales', '*.yml')]
-  register Sinatra::I18n
+  def test?
+    ENV['RACK_ENV'] == 'test'
+  end
 
-  Dotenv.load
+  def production?
+    ENV['RACK_ENV'] == 'production'
+  end
+
+  def root
+    @root ||= File.dirname(__FILE__)
+  end
+
+  def logger
+    return @logger if @logger
+
+    logger = production? ? Syslogger.new('bandejao') : Logger.new(STDOUT)
+    @logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  def save_file(university, content)
+    # In the real world, we would send this to S3
+    dir_files = "#{root}/log/parsers/#{university.name}-files"
+    FileUtils.mkdir_p(dir_files) unless File.directory? dir_files
+    date_format = DateTime.now.strftime('%Y-%m-%d_%H:%M:%S')
+    File.write "#{dir_files}/#{date_format}", content.to_s
+  end
 end
+
+Dotenv.load
+
+locales = Dir[File.join(App.root, 'config', 'locales', '*.yml')]
+I18n.backend.load_translations(locales)
--- bootstrap.rb
+++ bootstrap.rb
@@ -1,17 +1,14 @@
-# We could use Bundler.require, but I guess that explicit requires fits better our application
-# http://myronmars.to/n/dev-blog/2012/12/5-reasons-to-avoid-bundler-require
-
 require 'sinatra'
-require 'sinatra/respond_with'
 require 'sinatra/activerecord'
-require 'sinatra/i18n'
+require 'i18n'
 
 require 'dotenv'
 
+require './app.rb'
+
 Dir.glob('./models/*.rb') { |file| require file }
+Dir.glob('./lib/*.rb') { |file| require file }
 Dir.glob('./jobs/*.rb') { |file| require file }
 Dir.glob('./controllers/*.rb') { |file| require file }
 
 require './parsers/parsers'
-
-require './app.rb'
