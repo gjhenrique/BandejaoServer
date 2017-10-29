bb5e23a8992d0cb64a46d1380c982432d5a63ca2
Guilherme Henrique <gjhenrique@gmail.com>
2017-10-29 02:21:57 -0200

    Small refactorings...

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
-set :environment_variable, "RACK_ENV"
-set :output, {error: 'log/parser_error.log', standard: 'log/parser_success.log'}
+set :environment_variable, 'RACK_ENV'
+set :output, error: 'log/parser_error.log', standard: 'log/parser_success.log'
 
 every 1.hour do
-  rake "parsers:parse_universities"
+  rake 'parsers:parse_universities'
 end
--- models/university.rb
+++ models/university.rb
@@ -6,36 +6,30 @@ class University < ActiveRecord::Base
     order('RANDOM()')
   end
 
-  def has_campus?
-    !universities.empty?
-  end
-
-  def campus?
-    !university.nil?
-  end
-
-  def main_name
-    if university.nil?
-      name
-    else
-      university.name
-    end
-  end
-
   def self.by_name(name)
     where('LOWER(name) = ?', name.downcase).first
   end
 
   def self.find_campus
-    where.not(id: University.select(:university_id).where.not(university_id: nil).uniq)
-  end
-
-  def self.without_campus
-    where(university_id: nil)
+    where.not(id: University.select(:university_id)
+         .where.not(university_id: nil).uniq)
   end
 
   def eql?(other)
     other.name == name
+  end
+
+  # TODO: Check this
+  def has_campus?
+    universities.present?
+  end
+
+  def campus?
+    university.present?
+  end
+
+  def main_name
+    (university.try(:name) || name).downcase
   end
 
   def hash
